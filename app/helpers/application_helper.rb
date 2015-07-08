ad7f921a8cb661bade489fb3cb92085dd0be1ecb
sunsplat <heeycutie@yahoo.com>
2015-07-08 20:05:30 +0000

    changed routes.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
     def full_title(page_title)
   base_title = "Ruby on Rails Tutorial Sample App"
-  if page_title.empty?
+      if page_title.empty?
     base_title
-  else
+      else
     "#{base_title} | #{page_title}"
-  end
- end
+      end
+    end
 end
