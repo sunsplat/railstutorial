3f4f977bd1a6ec907d84a16aaa3af5d4d1ee0b7a
sunsplat <heeycutie@yahoo.com>
2015-07-11 17:22:29 +0000

    finally fixed 5.38 filling-in-layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,10 @@
 module ApplicationHelper
-    def full_title(page_title)
+    def full_title(page_title = '')
      base_title = "Ruby on Rails Tutorial Sample App"
       if page_title.empty?
        base_title
       else
-<<<<<<< HEAD
-    "#{page_title} | #{base_title}"
-=======
        "#{page_title} | #{base_title}"
->>>>>>> filling-in-layout
       end
     end
 end
