9c0582f40d843cc1ee9bc1a25330aa362b730702
sunsplat <heeycutie@yahoo.com>
2015-07-11 21:40:42 +0000

    merge application_helper error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,10 @@
 module ApplicationHelper
-    def full_title(page_title = '')
-<<<<<<< HEAD
+  def full_title(page_title= '')
      base_title = "Ruby on Rails Tutorial Sample App"
       if page_title.empty?
        base_title
       else
        "#{page_title} | #{base_title}"
       end
-=======
-      base_title = "Ruby on Rails Tutorial Sample App"
-        if page_title.empty?
-          base_title
-        else
-          "#{page_title} | #{base_title}"
-        end
->>>>>>> modeling-users
-    end
+  end
 end
