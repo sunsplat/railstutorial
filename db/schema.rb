afac47f39add87e4a58409796f597c42b8eb1b0d
sunsplat <heeycutie@yahoo.com>
2015-07-20 19:01:56 +0000

    a bunch of changes related to psasword_digest

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709195838) do
+ActiveRecord::Schema.define(version: 20150720185110) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password"
+    t.string   "password_digest"
+    t.string   "string"
   end
 
 end
