7dab0d00523b16b12a9d58dc2a8dc0c692668541
sunsplat <heeycutie@yahoo.com>
2015-07-20 19:39:03 +0000

    problem with password field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720185110) do
+ActiveRecord::Schema.define(version: 20150720193058) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150720185110) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "string"
+    t.string   "password"
   end
 
 end
