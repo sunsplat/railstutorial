28002b9a513f74536e9a5a21d7f86a8c1ffadb89
sunsplat <heeycutie@yahoo.com>
2015-07-27 20:50:44 +0000

    clean up user_test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -57,19 +57,8 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
-<<<<<<< HEAD
-end
-=======
-  
-   test "password should be present (nonblank)" do
-    @user.password = @user.password_confirmation = " " * 6
-    assert_not @user.valid?
-  end
 
-  test "password should have a minimum length" do
-    @user.password = @user.password_confirmation = " " * 6
-    assert_not @user.valid?
-  end
-  
 end
->>>>>>> signup
+
+
+
