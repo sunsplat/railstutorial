5c7ff2e49f6ef923614f7672c1805281a0cb8d7e
sunsplat <heeycutie@yahoo.com>
2015-07-27 20:55:53 +0000

    clean up merging

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,6 @@
 module ApplicationHelper
-<<<<<<< HEAD
+
   def full_title(page_title= '')
-=======
-    def full_title(page_title = "")
->>>>>>> signup
      base_title = "Ruby on Rails Tutorial Sample App"
       if page_title.empty?
        base_title
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150709222119) do
-=======
 ActiveRecord::Schema.define(version: 20150720193058) do
->>>>>>> signup
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -23,11 +18,9 @@ ActiveRecord::Schema.define(version: 20150720193058) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
-<<<<<<< HEAD
-=======
+
     t.string   "string"
     t.string   "password"
->>>>>>> signup
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
