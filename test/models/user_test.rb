38883a7ab8684b6d01c5e5d9e15d41c001dbd188
sunsplat <heeycutie@yahoo.com>
2015-07-27 20:12:58 +0000

    updated application helper

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -19,4 +19,15 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "     "
     assert_not @user.valid?
   end
+  
+   test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+  
 end
