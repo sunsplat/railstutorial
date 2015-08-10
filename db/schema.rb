b4588d77536d7d608eb8ca69d8284a499fd77055
sunsplat <heeycutie@yahoo.com>
2015-08-10 16:43:04 +0000

    first commit for loginlogout branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 20150720193058) do
+
+ActiveRecord::Schema.define(version: 20150709222119) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -18,9 +19,6 @@ ActiveRecord::Schema.define(version: 20150720193058) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
-
-    t.string   "string"
-    t.string   "password"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
